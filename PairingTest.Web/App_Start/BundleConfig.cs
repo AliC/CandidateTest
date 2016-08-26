3bf0d4fd140ce211e45f9b0713f59f9b71d84d5a
Alastair Danley-Cox <alastair_cox@btinternet.com>
2016-08-26 07:37:35 +0100

    Cleaned up BundleConfig Moved site css after bootstrap css Removed !important declaration now it's redundant

--- PairingTest.Web/App_Start/BundleConfig.cs
+++ PairingTest.Web/App_Start/BundleConfig.cs
@@ -10,28 +10,12 @@ namespace PairingTest.Web.App_Start
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                 "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                "~/Scripts/jquery-ui-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.unobtrusive*",
-                "~/Scripts/jquery.validate*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-               "~/Content/site.css",
-               "~/Content/bootstrap.css"));
+               "~/Content/bootstrap.css",
+                "~/Content/site.css"));
         }
     }
 }
